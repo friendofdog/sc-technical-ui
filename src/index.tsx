860b172fee03485eb517a0950be85da9c0e85d03
Kevin Kee <kevinkee9@gmail.com>
2025-04-06 21:31:41 +0900

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,94 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import "./styles.css";
+
+// Implement a feature to allow item selection with the following requirements:
+// 1. Clicking an item selects/unselects it.
+// 2. Multiple items can be selected at a time.
+// 3. Make sure to avoid unnecessary re-renders of each list item in the big list (performance).
+// 4. Currently selected items should be visually highlighted.
+// 5. Currently selected items' names should be shown at the top of the page.
+//
+// Feel free to change the component structure at will.
+
+type AppProps = {
+  items: Item[];
+};
+
+function App({ items }: AppProps) {
+  return (
+    <ul className="List">
+      {items.map((item) => (
+        <li
+          key={item.name}
+          className="List-item"
+          style={{ backgroundColor: item.color }}
+        >
+          {item.name}
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+// ---------------------------------------
+// Do NOT change anything below this line.
+// ---------------------------------------
+
+type Item = {
+  name: string;
+  color: string;
+};
+
+const sizes = ["tiny", "small", "medium", "large", "huge"];
+const colors = [
+  "navy",
+  "blue",
+  "aqua",
+  "teal",
+  "olive",
+  "green",
+  "lime",
+  "yellow",
+  "orange",
+  "red",
+  "maroon",
+  "fuchsia",
+  "purple",
+  "silver",
+  "gray",
+  "black",
+];
+const fruits = [
+  "apple",
+  "banana",
+  "watermelon",
+  "orange",
+  "peach",
+  "tangerine",
+  "pear",
+  "kiwi",
+  "mango",
+  "pineapple",
+];
+
+const items = sizes.flatMap((size) =>
+  fruits.flatMap((fruit) =>
+    colors.flatMap((color) => [
+      {
+        name: `${size} ${color} ${fruit}`,
+        color,
+      },
+    ])
+  )
+);
+
+const rootElement = document.getElementById("root")!;
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
+  <React.StrictMode>
+    <App items={items} />
+  </React.StrictMode>
+);
